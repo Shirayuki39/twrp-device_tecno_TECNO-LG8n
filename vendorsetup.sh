93400ce283dbb232af680dc3cc025fcbfa15db8a
Ren <89468157+Shirayuki39@users.noreply.github.com>
2024-05-08 23:18:34 +0800

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,1 +1,1 @@
-git clone https://github.com/akifakif32/twrp-device_transsion_mt6789-common.git -b fox_12.1 device/transsion/mt6789-common
+git clone https://github.com/Shirayuki39/twrp-device_transsion_mt6789-common.git -b fox_12.1 device/transsion/mt6789-common
